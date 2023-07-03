f648974447291d2653b04dcfbeebb834860db812
Zefanja Timorason <z.timorason@gmail.com>
2023-07-03 19:35:40 +0200

    feed page is finished, Create and Read functionality works now

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [allPosts, setAllPosts] = useState([]);
+
+  const handleSearchChange = (event) => {};
+  const handleTagClick = () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      console.log(data);
+      setAllPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-      <b>Feed</b>
-    </div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+    </section>
   );
 };
 
