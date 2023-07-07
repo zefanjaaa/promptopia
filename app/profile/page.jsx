ff7e34737c14724b2a4f961cda615c63f8227505
Zefanja Timorason <z.timorason@gmail.com>
2023-07-07 14:36:06 +0200

    finished all CRUD operations, they are all working properly

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -21,10 +21,26 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {
+  const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        const filteredPosts = myPosts.filter((p) => p._id !== post._id);
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <Profile
       name="My"
---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
+
+const EditPrompt = () => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: " ",
+    tag: " ",
+  });
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+
+      const data = await response.json();
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
+  const UpdatePrompt = async (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    if (!promptId) return alert("Prompt ID not found");
+
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        alert("Prompt edited successfully!");
+        router.push("/profile");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={UpdatePrompt}
+    />
+  );
+};
+
+export default EditPrompt;
